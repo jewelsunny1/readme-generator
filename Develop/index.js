10a1219c54fba8240fb9eaa4548771f4eceb1dbe
Jewel <jewelsunnyk@gmail.com>
2024-04-24 07:57:49 -0500

    Updated the content of each file to be able to generate a readme file

--- Develop/index.js
+++ Develop/index.js
@@ -1,14 +1,83 @@
 // TODO: Include packages needed for this application
+//inquirer is a module(file ) of node that allows you to use functions to create prompts and interact with user//
+//remember require is like adding a script tag? and think import when you see require//
+
+//const inquirer= require('inquirer');// inthis case what we put in require should match the const name
+import inquirer from 'inquirer';
+
 
 // TODO: Create an array of questions for user input
-const questions = ['Title of project:','Description','Table of Contents',
-'Installation','Usage','License','Contributing','Tests','Questions'];
+//question object is part of inquirer module//it is hass, meanig it has KEY-VALUE pairs
+const questions = [
+{
+    type:'input',
+    name:'title',
+    message:'What is the title of your application?'
+},
+{
+    type:'input',
+    name:'description',
+    message:'Provide a description of your application',
+},
+{
+    type:'input',
+    name:'installation',
+    message:'How should users install your application?',
+},
+{
+    type:'input',
+    name:'usage',
+    message:'Provide instructions on how your application can be used?',
+},
+{
+    type:'list',//chose list bc there are various licenses to choose from//
+    name:'license',
+    message:'Choose a license for your application: ',
+    choices:['MIT','Apache-2.0','GPL-3.0','None']//Choices array or a function returning a choices array. If defined as a function, the first parameter will be the current inquirer session answers. //
+},
+{
+    type:'input',
+    name:'contributing',
+    message:'How can others contribute to you appication?',
+},
+{
+    type:'input',
+    name:'tests',
+    message:'How should users run tests for you application?',
+},
+];
+    
+    
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// remember prior to function we have to bring in the fs module of node to use writeTo
+//this function takes in two arguments, fileName and Data
+const fs= require('fs');
+const generateMarkdown = require('./utilis/generateMarkdown');
+
+//const { default: inquirer } = require('inquirer');
+//the parameters are fileName: name of file to write to and data:the data to be written into the file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(err)=> {//remember writeFile takes in 3 parameters)
+    err? console.log('Error writing README file:',err): console.log('README file was created successfully!');
+     });
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+ inquirer
+ //prompt initiates the process of prompting the user with question defined in 'questions' array
+ .prompt(questions)//.prompt is a method of inquirer that is being applied ot questioin object(this is a object of inquirer!)
+ //remeber the prompt method returns a PROMISE object. a promise is made b/w prompt and then!
+//.then is method called when user has answered all questions.//it recieves the user's answers as a parameter(answers), then you can proceed to handle these answers
+ .then(answers=>{//markdown content for READme is generated based on users' answers to the questions
+    const readmeContent= generateMarkdown(answers);
+    writeToFile('README.md,readmeContent');//calling the writeToFile func f/o earlier to write genrated content to a file named 'README.md'
+ })
+ .catch(error=>{
+    console.log('Error',error);
+ });
 
+}
 // Function call to initialize app
 init();
--- Develop/utilis/generateMarkdown.js
+++ Develop/utilis/generateMarkdown.js
@@ -1,20 +1,79 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+//what are these license badges?The badges do not fully replace the license informations for your projects, they are only emblems for the README, that the user can see the License at first glance.
+//more info on badges can be found in this link: https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license){
+    return''
+  
+  }
+  else{ //returns a markdown badge with the license type
+    return`[![License:${license}](https://img.shields.io/badge/License-${license}-blue.svg)](https://opensource.org/licenses/${license})`;
+    
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if(!license){
+    return ''
+  }
+  else{//typical link to licenses to open source projects
+    return `https://opensource.org/licenses/${license}`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(!license){
+    return ''
+  }
+  else{//states application is licensed under a specific type of license, with clickable link that takes user to the selected license page to read more about it
+    return `This application is licensed under [${license}](${renderLicenseLink(license)}) license.`;
+    //render license link in this creates a clickable link when user lick on the specified license type they chose.
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  
+  ## Installation
+
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${data.license}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Tests
+
+ ${data.tests}
+
+  `;
 }
 
 module.exports = generateMarkdown;
